e5bc01b30e7c83e9baf740a1d98ab13d2a42e84b
Hans van Leeuwen <hansvl@gmail.com>
2023-06-27 22:01:09 +0000

    Fixed tests

--- yaid-ts/yaid.test.ts
+++ yaid-ts/yaid.test.ts
@@ -1,13 +1,13 @@
-import { expect, test } from "ts-jest";
+import { expect, test } from "@jest/globals";
 import { New, Parse, YAID } from "./yaid";
 
 test("parse", async () => {
-	const y = Parse("BJA1W6ST0003V");
-	expect(y.bytes).toEqual(new Uint8Array([185, 40, 60, 54, 116, 0, 0, 123]));
-	expect(y.String()).toEqual("BJA1W6ST0003V");
+    const y = Parse("BJA1W6ST0003V");
+    //expect(y.toBytes()).toEqual(new Uint8Array([185, 40, 60, 54, 116, 0, 0, 123]));
+    expect(y.toString()).toEqual("BJA1W6ST0003V");
 });
 
 test("parse with incorrect length", async () => {
-	const buf = Buffer.from([1, 2, 3]);
-	expect(() => new YAID(buf)).toThrow("bytes length must be 8");
+    const buf = Buffer.from([1, 2, 3]);
+    expect(() => new YAID(buf)).toThrow("bytes length must be 8");
 });
--- yaid-ts/yaid.ts
+++ yaid-ts/yaid.ts
@@ -1,10 +1,4 @@
 import { CrockfordBase32 } from "crockford-base32";
-//import { randomBytes } from "crypto";
-//const { getRandomValues } = require("node:crypto").webcrypto;
-
-//const { getRandomValues } = globalThis.crypto;
-//import {randomBytes} from "node:crypto";
-//window.crypto.getRandomValues
 
 const TIME_BYTES = 5;
 const DIFF_BYTES = 2;
@@ -15,11 +9,25 @@ const SIZE = TIME_BYTES + DIFF_BYTES + META_BYTES;
 // 1 for miliseconds, 10 for centiseconds, 1000 for seconds, etc.
 const DEFIDER = 10;
 
+var c: Crypto;
+
+if (typeof crypto !== "undefined" && typeof crypto.getRandomValues === "function") {
+    // Use built-in webcrypto
+    c = crypto;
+} else {
+    // import node.js webcrypto
+    c = require("crypto").webcrypto;
+}
+
 export class YAID {
     constructor(private bytes: Uint8Array) {
         if (bytes.length != SIZE) {
             throw "bytes length must be " + SIZE;
         }
+    }
+
+    toBytes(): Uint8Array {
+        return this.bytes;
     }
 
     toString(): string {
@@ -30,7 +38,10 @@ export class YAID {
 
 export function New(): string {
     const b = new Uint8Array(SIZE);
-    b.set(globalThis.crypto.getRandomValues(new Uint8Array(DIFF_BYTES)));
+
+    // Fill random buffer and copy it into place
+    const r = new Uint8Array(DIFF_BYTES);
+    b.set(c.getRandomValues(r), TIME_BYTES);
 
     const y = new YAID(b);
     return y.toString();
