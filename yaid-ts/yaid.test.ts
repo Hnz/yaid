8db1a2648c38b25a6d729fccb1a9fc776bd0a4aa
Hans van Leeuwen <hansvl@gmail.com>
2023-06-27 23:01:52 +0000

    include crockford base32 implementation

--- yaid-ts/yaid.test.ts
+++ yaid-ts/yaid.test.ts
@@ -3,7 +3,7 @@ import { New, Parse, YAID } from "./yaid";
 
 test("parse", async () => {
     const y = Parse("BJA1W6ST0003V");
-    //expect(y.toBytes()).toEqual(new Uint8Array([185, 40, 60, 54, 116, 0, 0, 123]));
+    expect(y.toBytes()).toEqual(new Uint8Array([185, 40, 60, 54, 116, 0, 0, 123]));
     expect(y.toString()).toEqual("BJA1W6ST0003V");
 });
 
--- yaid-ts/yaid.ts
+++ yaid-ts/yaid.ts
@@ -1,4 +1,4 @@
-import { CrockfordBase32 } from "crockford-base32";
+import { base32Decode, base32Encode } from "./crockford";
 
 const TIME_BYTES = 5;
 const DIFF_BYTES = 2;
@@ -9,7 +9,7 @@ const SIZE = TIME_BYTES + DIFF_BYTES + META_BYTES;
 // 1 for miliseconds, 10 for centiseconds, 1000 for seconds, etc.
 const DEFIDER = 10;
 
-var c: Crypto;
+let c: Crypto;
 
 if (typeof crypto !== "undefined" && typeof crypto.getRandomValues === "function") {
     // Use built-in webcrypto
@@ -21,8 +21,8 @@ if (typeof crypto !== "undefined" && typeof crypto.getRandomValues === "function
 
 export class YAID {
     constructor(private bytes: Uint8Array) {
-        if (bytes.length != SIZE) {
-            throw "bytes length must be " + SIZE;
+        if (bytes.byteLength != SIZE) {
+            throw new Error("bytes length must be " + SIZE);
         }
     }
 
@@ -32,34 +32,20 @@ export class YAID {
 
     toString(): string {
         const b = Buffer.from(this.bytes);
-        return CrockfordBase32.encode(b);
+        return base32Encode(b);
     }
 }
 
-export function New(): string {
+export function New(): YAID {
     const b = new Uint8Array(SIZE);
 
     // Fill random buffer and copy it into place
     const r = new Uint8Array(DIFF_BYTES);
     b.set(c.getRandomValues(r), TIME_BYTES);
 
-    const y = new YAID(b);
-    return y.toString();
+    return new YAID(b);
 }
 
 export function Parse(yaid: string): YAID {
-    return new YAID(CrockfordBase32.decode(yaid));
+    return new YAID(base32Decode(yaid));
 }
-/*
-async function randomFactory(): Promise<(size: number) => Buffer> {
-	if (randomBytes) {
-		return randomBytes;
-	} else if (crypto && crypto.getRandomValues) {
-		return function (size: number): Buffer {
-			return crypto.getRandomValues(Buffer.alloc(size));
-		};
-	} else {
-		throw "Could not find a secure random source";
-	}
-}
-*/
