63a3456568daa35d7dcfaa30666ef4bbf8eecd9a
Hans van Leeuwen <hansvl@gmail.com>
2023-06-28 16:33:37 +0000

    Update

--- yaid-ts/yaid.test.ts
+++ yaid-ts/yaid.test.ts
@@ -3,8 +3,8 @@ import { New, Parse, YAID } from "./yaid";
 
 test("parse", async () => {
     const y = Parse("BJA1W6ST0003V");
-    expect(y.toBytes()).toEqual(new Uint8Array([185, 40, 60, 54, 116, 0, 0, 123]));
     expect(y.toString()).toEqual("BJA1W6ST0003V");
+    expect(y.toBytes()).toEqual(new Uint8Array([92, 148, 30, 27, 58, 0, 0, 61]));
 });
 
 test("parse with incorrect length", async () => {
--- yaid-ts/yaid.ts
+++ yaid-ts/yaid.ts
@@ -9,21 +9,66 @@ const SIZE = TIME_BYTES + DIFF_BYTES + META_BYTES;
 // 1 for miliseconds, 10 for centiseconds, 1000 for seconds, etc.
 const DEFIDER = 10;
 
-let c: Crypto;
+const max_timestamp = 1099511627775;
 
-if (typeof crypto !== "undefined" && typeof crypto.getRandomValues === "function") {
-    // Use built-in webcrypto
-    c = crypto;
-} else {
-    // import node.js webcrypto
+let c: Crypto = globalThis.crypto || crypto;
+
+// NodeJS < 20 does not support globalThis.
+// Import node.js webcrypto
+if (!c) {
     c = require("crypto").webcrypto;
 }
 
 export class YAID {
     constructor(private bytes: Uint8Array) {
         if (bytes.byteLength != SIZE) {
-            throw new Error("bytes length must be " + SIZE);
+            throw new RangeError("bytes length must be " + SIZE);
         }
+    }
+
+    meta(): number {
+        return this.bytes[SIZE];
+    }
+
+    setMeta(i: number) {
+        if (i < 0 || i > 255) {
+            throw new RangeError("meta must be within 0-255");
+        }
+        this.bytes[SIZE] = i;
+    }
+
+    time(): Date {
+        const ms = this.timestamp() * DEFIDER;
+        return new Date(ms);
+    }
+
+    setTime(t: Date) {
+        const cs = t.getTime() / DEFIDER;
+        this.setTimestamp(cs);
+    }
+
+    // Time as hundredth of a second since January 1, 1970 12:00:00 AM UTC
+    timestamp(): number {
+        return (
+            this.bytes[4] |
+            (this.bytes[3] << 8) |
+            (this.bytes[2] << 16) |
+            (this.bytes[1] << 24) |
+            (this.bytes[0] << 32)
+        );
+    }
+
+    // Set the time as hundredth of a second since January 1, 1970 12:00:00 AM UTC
+    setTimestamp(t: number) {
+        if (t > max_timestamp) {
+            throw new Error("timestamp must not be greater than " + max_timestamp);
+        }
+
+        this.bytes[0] = t >> 32;
+        this.bytes[1] = t >> 24;
+        this.bytes[2] = t >> 16;
+        this.bytes[3] = t >> 8;
+        this.bytes[4] = t;
     }
 
     toBytes(): Uint8Array {
@@ -43,7 +88,9 @@ export function New(): YAID {
     const r = new Uint8Array(DIFF_BYTES);
     b.set(c.getRandomValues(r), TIME_BYTES);
 
-    return new YAID(b);
+    const y = new YAID(b);
+    y.setTime(new Date());
+    return y;
 }
 
 export function Parse(yaid: string): YAID {
