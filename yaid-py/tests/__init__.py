518d4c611f2c75163747aeefa55b3cecfcc88680
Hans van Leeuwen <hansvl@gmail.com>
2023-07-04 21:52:49 +0000

    Python coming to life

--- yaid-py/tests/__init__.py
+++ yaid-py/tests/__init__.py
@@ -1,27 +1,39 @@
-
 import unittest
 import doctest
 import yaid
 
+
 def load_tests(loader, tests, ignore):
+    """Run documentation tests"""
     tests.addTests(doctest.DocTestSuite(yaid))
     return tests
 
-class TestStringMethods(unittest.TestCase):
 
+class TestStringMethods(unittest.TestCase):
     def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
+        self.assertEqual("foo".upper(), "FOO")
 
     def test_isupper(self):
-        self.assertTrue('FOO'.isupper())
-        self.assertFalse('Foo'.isupper())
+        self.assertTrue("FOO".isupper())
+        self.assertFalse("Foo".isupper())
 
-    def test_split(self):
-        s = 'hello world'
-        self.assertEqual(s.split(), ['hello', 'world'])
-        # check that s.split fails when the separator is not a string
-        with self.assertRaises(TypeError):
-            s.split(2)
+    def test_timestamp(self):
+        y = yaid.YAID()
+        self.assertEqual(y.bytes, bytearray([0, 0, 0, 0, 0, 0, 0, 0]))
+        self.assertEqual(y.timestamp(), 0)
 
-if __name__ == '__main__':
+        y.set_timestamp(1)
+        self.assertEqual(y.bytes, bytearray([0, 0, 0, 0, 1, 0, 0, 0]))
+        self.assertEqual(y.timestamp(), 1)
+
+        y.set_timestamp(32769)
+        self.assertEqual(y.bytes, bytearray([0, 0, 0, 128, 1, 0, 0, 0]))
+        self.assertEqual(y.timestamp(), 32769)
+
+        y.set_timestamp(yaid.MAX_TIMESTAMP)
+        self.assertEqual(y.bytes, bytearray([255, 255, 255, 255, 255, 0, 0, 0]))
+        self.assertEqual(y.timestamp(), yaid.MAX_TIMESTAMP)
+
+
+if __name__ == "__main__":
     unittest.main()
--- yaid-py/yaid/__init__.py
+++ yaid-py/yaid/__init__.py
@@ -3,6 +3,8 @@ YAID package
 
 bla die bla.
 
+Example of low-level usage:
+
 >>> from yaid import YAID
 >>> from datetime import datetime
 >>> y = YAID()
@@ -13,10 +15,10 @@ bla die bla.
 123
 >>> print(y)
 000000000007P
->>> d = datetime(2222, 1, 2, 3, 4, 5, 6)
+>>> d = datetime(2222, 1, 2, 3, 4, 5, 54321)
 >>> y.set_time(d)
 >>> y.time()
-datetime.datetime(2222, 1, 2, 3, 4, 5, 6)
+datetime.datetime(2222, 1, 2, 3, 4, 5, 50000)
 """
 
 import krock32
@@ -32,45 +34,61 @@ SIZE: int = TIME_BYTES + DIFF_BYTES + META_BYTES
 
 DEFIDER: int = 10
 
+""" Maximum value of the timestamp """
 MAX_TIMESTAMP: int = 1099511627775
 
+
 class YAID:
+    """Yet Another ID"""
+
     bytes: bytearray
 
-    def __init__(self, bytes_array= bytearray([0,0,0,0,0,0,0,0])) -> None:
-        if len(bytes_array) != SIZE:
-            raise ValueError(f"bytes length must be {SIZE} not {len(bytes_array)}")
-        self.bytes = bytes_array
+    def __init__(self, bytes=bytearray(b"\x00\x00\x00\x00\x00\x00\x00\x00")) -> None:
+        if len(bytes) != SIZE:
+            raise ValueError(f"bytes length must be {SIZE} not {len(bytes)}")
+        self.bytes = bytearray(bytes)
 
     def differentiator(self) -> bytearray:
-        return self.bytes[TIME_BYTES:TIME_BYTES + DIFF_BYTES]
+        return self.bytes[TIME_BYTES : TIME_BYTES + DIFF_BYTES]
 
     def set_differentiator(self, d: bytearray) -> None:
-        self.bytes[TIME_BYTES:TIME_BYTES + DIFF_BYTES] = d
+        self.bytes[TIME_BYTES : TIME_BYTES + DIFF_BYTES] = d
 
     def meta(self) -> int:
+        """
+        Return the meta byte
+        """
         return self.bytes[TIME_BYTES + DIFF_BYTES]
 
     def set_meta(self, i: int) -> None:
+        """
+        Set the meta byte. Value must be between 0-255.
+        """
         if i < 0 or i > 255:
             raise ValueError("meta must be within 0-255")
         self.bytes[TIME_BYTES + DIFF_BYTES] = i
 
     def time(self) -> datetime:
-        ms: int = self.timestamp() * DEFIDER
+        ms = self.timestamp() * DEFIDER
         return datetime.utcfromtimestamp(ms / 1000.0)
 
     def set_time(self, t: datetime) -> None:
-        cs: int = t.timestamp() * DEFIDER
-        self.set_timestamp(cs)
+        ts = int(t.timestamp() * 1000.0 / DEFIDER)
+        self.set_timestamp(ts)
 
     def timestamp(self) -> int:
+        """
+        Time as hundredth of a second since January 1, 1970 12:00:00 AM UTC
+        """
         t: int = 0
         for i in range(TIME_BYTES):
             t += self.bytes[TIME_BYTES - 1 - i] << (i * 8)
         return t
 
     def set_timestamp(self, t: int) -> None:
+        """
+        Set the time as hundredth of a second since January 1, 1970 12:00:00 AM UTC
+        """
         t = int(t)
         if t > MAX_TIMESTAMP:
             raise ValueError(f"timestamp must not be greater than {MAX_TIMESTAMP}")
@@ -83,12 +101,12 @@ class YAID:
         return encoder.finalize()
 
 
-def new(meta: Optional[int] = None, size: int = 8, time: Optional[datetime] = None) -> YAID:
+def new(meta: Optional[int] = None, size: int = 8, time=datetime.utcnow()) -> YAID:
     if meta is None:
         meta = _random_bytes(META_BYTES)[0]
 
     y = YAID(bytearray(SIZE))
-    y.set_time(time or datetime.utcnow())
+    y.set_time(time)
     y.set_differentiator(_random_bytes(DIFF_BYTES))
     y.set_meta(meta)
 
@@ -97,7 +115,8 @@ def new(meta: Optional[int] = None, size: int = 8, time: Optional[datetime] = No
 
 def parse(yaid: str) -> YAID:
     decoder = krock32.Decoder(strict=True, checksum=False)
-    b: bytes = decoder.update(yaid)
+    decoder.update(yaid)
+    b = decoder.finalize()
     return YAID(bytearray(b))
 
 
